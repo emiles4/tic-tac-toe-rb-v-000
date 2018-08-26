dd16a19e29859ab24245d70f4e649353f082ea67
Emily Miles <emiles4@gmail.com>
2018-08-26 04:02:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,7 +94,7 @@ WIN_COMBINATIONS = [
   [2,4,6]  # Right diagonal
 ]
 
-def won?(board)
+##def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
